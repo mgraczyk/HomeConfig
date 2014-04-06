fa9dd1734e2a1e7df4ef3702f897d7adb6b1a422
mgraczyk@qti.qualcomm.com <mgraczyk@qti.qualcomm.com>
2014-04-05 22:29:27 -0500

    Added some data collection scripts

---
+++ scripts/collect_stats.py
@@ -0,0 +1,174 @@
+#!/usr/bin/python3
+
+import pmu_stats
+
+import sys
+import os
+import json
+import numpy as np
+
+from collections import defaultdict
+from collections import namedtuple
+from functools import reduce
+from itertools import chain
+from itertools import repeat
+from itertools import starmap
+from operator import getitem
+from operator import itemgetter
+
+import pprint as pp
+
+def get_immediate_subdirectories(dr):
+    return (name for name in os.listdir(dr)
+            if os.path.isdir(os.path.join(dr, name)))
+
+class Autolist(list):
+    def __init__(self, defaultFactory, filler=None):
+        super().__init__(self)
+
+        self._filler = filler
+        self._defaultFactory = defaultFactory
+        self._isOn = lambda: Control
+
+    def __setitem__(self, key, value):
+        try:
+            if key >= len(self):
+                self.extend([self._filler]*(key - len(self) + 1))
+        except TypeError as e:
+            pass
+
+        super().__setitem__(key, value)
+
+    def __getitem__(self, key):
+        try:
+            if key >= len(self):
+                self[key] = self._defaultFactory()
+        except TypeError as e:
+            pass
+
+        return super().__getitem__(key)
+
+def recursive_autolist():
+    return Autolist(recursive_autolist, None)
+
+class IndexDict(dict):
+    def __missing__(self, key):
+        super().__setitem__(key, len(self))
+        return len(self) - 1
+
+def get_stats(path):
+    PmuStatFileName = "pmu_stats.txt"
+
+    try: 
+        return pmu_stats.parse_pmu_file(os.path.join(path, PmuStatFileName))
+    except Exception as eOuter:
+        # It could be test subdirectory if the test "failed"
+        try:
+            return pmu_stats.parse_pmu_file(os.path.join(path, "test", PmuStatFileName))
+        except Exception as eInner:
+            # Show the first error since it will be more meaningful
+            raise eInner from eOuter
+
+def parse_passname(passname, dimensions):
+    values = []
+    rem = passname
+    for d in dimensions:
+        pos = rem.find(d)
+        if pos < 0:
+            raise ValueError("Passname {} does not contain dimension {}".format(passname, d))
+        if rem[pos-1] != "_":
+            raise ValueError("Bad passname {}".format(passname))
+
+        # The value better not have an underscore in it...
+        nameEnd = pos + len(d)
+        valueEnd = rem.find("_", nameEnd)
+        if valueEnd < 0:
+            valueEnd = len(rem)
+        values.append(rem[nameEnd:valueEnd])
+
+        # Remove leading underscore
+        rem = rem[0:pos-1] + rem[valueEnd:]
+
+    return rem, values
+
+def normalize_type(values):
+    typePreferences = (lambda x: int(x,0), float)
+    for t in typePreferences:
+        try:
+            return tuple(map(t, values))
+        except (SyntaxError, ValueError):
+            pass
+
+    return tuple(values)
+
+def parse_values_from_results(passData, dimensions):
+    # Two extra for stats and test names
+    tree = recursive_autolist()
+    domainIdxs = [IndexDict() for i in range(len(dimensions) + 2)]
+
+    # Read all the data.
+    # We do this in a roundabout way since we don't know in advance
+    # What all of the data points will be.
+    for passName, values in passData:
+        test, domain = parse_passname(passName, dimensions)
+        for stat,value in values.items():
+            indices = list(map(getitem, domainIdxs, chain((test, stat), domain)))
+            leaf = reduce(getitem, indices[:-1], tree)
+            leaf[indices[-1]] = value
+
+    # Return the domain as a numeric type if possible
+    domainNames = tuple(chain(("test", "stat"), dimensions))
+    domainValues = tuple(map(normalize_type, [d.keys() for d in domainIdxs]))
+
+    return {
+        "domain_names": domainNames,
+        "domain_values": domainValues,
+        "data": tree
+    }
+
+def sort_data(keys, data):
+    """ Sorts a multidimensional array based on a list of keys.
+        The sort order of dimension i of data is determined by keys[i].
+    """
+
+    sortOrders = tuple(map(np.argsort, map(np.array, keys)))
+    dArr = np.array(data)
+    colon = slice(None)
+    dim = len(sortOrders)
+    for i, indices in enumerate(sortOrders):
+        indexer = [indices if j==i else colon for j in range(dim)]
+        dArr = dArr[indexer]
+
+    keysort = lambda l,i: tuple(l[p] for p in i)
+    sortedKeys = tuple(map(keysort, keys, sortOrders))
+
+    return sortedKeys, dArr.tolist()
+
+def collect_stats(path):
+    passData = []
+    for subdir in get_immediate_subdirectories(path):
+        try: 
+            passData.append((subdir, get_stats(os.path.join(path, subdir))))
+        except Exception as e:
+            # Warn but ignore
+            print("Couldn't get data for {}: {}".format(subdir, e))
+
+    # Since we read the data in an unspecified order, we need to
+    # sort each dimension to get a result that's easier to use
+    sweep = parse_values_from_results(passData, ["mmvec_vfifo_depth"])
+    sweep["domain_values"], sweep["data"] = sort_data(sweep["domain_values"], sweep["data"])
+
+    return sweep
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        path = sys.argv[1]
+    else:
+        path = os.getcwd()
+
+    json.dump(
+            collect_stats(path), sys.stdout,
+            separators=(',',':'),
+            check_circular=False,
+            allow_nan=False)
+    print()
---
+++ scripts/plot_stat.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python3
+
+import matplotlib
+matplotlib.use('WebAgg')
+import matplotlib.pyplot as plt
+
+import sys
+import json
+
+import numpy as np
+
+from collections import defaultdict
+from operator import itemgetter
+
+class Sweep(object):
+    def __init__(self, domainNames, domainValues, data, sliced=tuple()):
+       self._domainNames = domainNames
+       self._domainValues = domainValues
+       self._data = np.array(data, np.float64)
+       self._sliced = sliced
+
+    @classmethod
+    def FromJson(cls, json):
+        try:
+            return cls(json["domain_names"], json["domain_values"], json["data"])
+        except KeyError as e:
+            raise ValueError("Json data set is missing something.") from e
+        except Exception as e:
+            raise ValueError("Bad data in the data set.") from e
+
+    @property
+    def domain_names(self):
+        return self._domainNames
+
+    @property
+    def domain_values(self):
+        return self._domainValues
+
+    @property
+    def data(self):
+        return self._data
+
+    def slice_dimension(self, dimension, name):
+        try:
+            dimIdx = self.domain_names.index(dimension)
+        except ValueError as e:
+            raise ValueError("{} is not in the data set's domain.".format(dimension)) from e
+
+        positions = self.domain_values[dimIdx]
+
+        try:
+            testPos = positions.index(name)
+        except ValueError as e:
+            raise ValueError("{} '{}' is not in the data set".format(dimension, name))
+       
+        newDNames, newDValues = zip(*(v for i, v in
+            enumerate(zip(self.domain_names, self.domain_values))
+            if i != dimIdx))
+        indexer = [testPos if j == dimIdx else slice(None) for j in range(len(self.domain_names))]
+        sliced = self._sliced + ((dimension, name),)
+        return Sweep(newDNames, newDValues, self.data[indexer], sliced)
+
+    def slice_test(self, test):
+        return self.slice_dimension("test", test)
+
+    def slice_stat(self, stat):
+        return self.slice_dimension("stat", stat)
+
+    def plot(self):
+        x = self.domain_values[0]
+        y = self.data
+        
+        plt.plot(x, y)
+        plt.axis([min(x), max(x), min(y), max(y)])
+        plt.title(self._sliced)
+        plt.show()
+
+def example(path, test, stat):
+    with open(path, "r") as f:
+        rawData = json.load(f)
+
+    sweep = Sweep.FromJson(rawData)
+    sweep.slice_test(test).slice_stat(stat).plot()
+
+if __name__ == "__main__":
+    if len(sys.argv) < 4:
+        print("USAGE: {} data_file test_name stat_name".format(sys.argv[0]))
+        exit(1)
+    else:
+        example(*sys.argv[1:4])
