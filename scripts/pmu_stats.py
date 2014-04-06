19e194c836fa06d6dcf4bb27dae8d00be390b86d
Michael Graczyk <michael@mgraczyk.com>
2014-04-05 22:29:27 -0500

    Added some data collection scripts

---
+++ scripts/pmu_stats.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+import sys
+import json
+import re
+
+pmuRe = re.compile("(?:([^:]*):)?([^:]+):([^:]+)")
+
+def parse_pmu_fp(f):
+    values = {}
+    for num, line in enumerate(f):
+        match = pmuRe.search(line)
+        if match:
+            groups = match.groups()
+            # Sometimes the first capture group is missing :(
+            if len(groups) < 3:
+                raise ValueError("Pmu Stat missing data at line " + str(num))
+            values[groups[-2]] = int(groups[-1], 0)
+
+    return values
+
+def parse_pmu_file(filePath):
+    with open(filePath, 'r') as f:
+        return parse_pmu_fp(f)
+
+if __name__ == "__main__":
+    sys.stdout.write(json.dumps(parse_pmu_fp(sys.stdin), separators=(',',':')))
+    print()
