ec418fc9e5bdb354171412c84f7a92bbbb9de791
Michael Graczyk <michael@mgraczyk.com>
2014-07-11 19:47:50 -0500

    Added a few things to pktid script

--- scripts/parse_pktidlog.py
+++ scripts/parse_pktidlog.py
@@ -15,9 +15,9 @@ import numpy as np
 CommitEvent = namedtuple("CommitEvent", ["time","event","pc","tid","id"])
 PerfEvent = namedtuple("PerfEvent", ["time","event","cnt"])
 
-CommitSequence = namedtuple("CommitSequence", ["startTime","commits"])
+CommitSequence = namedtuple("CommitSequence", ["startTime","counts"])
 
-def get_sequence(data, eventName):
+def get_events(data, eventName):
     if not data[eventName]:
         return []
 
@@ -28,8 +28,14 @@ def get_sequence(data, eventName):
 
     for commit in data[eventName]:
         seq[commit.time - minTime] = 1
-    
-    retval = CommitSequence(minTime, np.cumsum(seq))
+
+    retval = CommitSequence(minTime, seq)
+    return retval
+
+def get_sequence(data, eventName):
+    events = get_events(data, eventName)
+    retval = CommitSequence(events.startTime, np.cumsum(events.counts))
+
     return retval
 
 def get_commit_sequence(data):
@@ -63,10 +69,34 @@ def _get_pktid_data_from_file(fp):
 
     return events
 
+def plot_sequence_spectrum(data, eventName, displayName): 
+    seq = get_sequence(data, eventName)
+    y = np.abs(-1j * np.fft.fft(seq.counts))
+    x = np.array(range(len(y)))
+    y = y*x
+
+    xMin = 0
+    xMax = len(y)/2
+    ySlice = y[xMin:xMax]
+    yMin, yMax = np.min(ySlice), np.max(ySlice)
+    argmax = np.argmax(ySlice)
+
+    if yMin == yMax:
+        yMin -= 1
+        yMax += 1
+
+    plt.plot(x, y)
+    plt.axis([xMin, xMax, yMin, yMax])
+    plt.title("{}s Spectrum: Argmax = {}".format(displayName, argmax))
+    plt.xlabel("\\omega")
+    plt.ylabel("|FFT()|")
+    plt.savefig("{}_spectrum.png".format(displayName))
+    plt.close()
+
 def plot_sequence(data, eventName, displayName):
     seq = get_sequence(data, eventName)
-    x = np.array(range(seq.startTime, seq.startTime + len(seq.commits)))
-    y = seq.commits
+    y = seq.counts
+    x = np.array(range(seq.startTime, seq.startTime + len(y)))
    
     xMin, xMax = x[0], x[-1]
     yMin, yMax = y[0], y[-1]
@@ -82,14 +112,15 @@ def plot_sequence(data, eventName, displayName):
     plt.xlabel("pcycles")
     plt.ylabel(displayName)
     plt.savefig("{}.png".format(displayName))
+    plt.close()
 
 def get_pktid_data(path):
     with open(path, "r") as fp:
         data = _get_pktid_data_from_file(fp)
 
     plot_sequence(data, "newpc", "Commit")
-    #plot_sequence(data, "250", "VStore_Miss")
     plot_sequence(data, "245", "COPROC_L2_STORE_STALL_CYCLES")
+    plot_sequence(data, "248", "COPROC_CORE_VFIFO_FULL_STALL")
 
 def main():
     if len(sys.argv) > 1:
