f9ad2aa9d01075a8fb2da41b3ce3002ce1292a8e
Michael Graczyk <michael@mgraczyk.com>
2014-09-08 18:26:40 -0700

    Fix matlab path on osx

--- .chromium_dev.sh
+++ .chromium_dev.sh
@@ -1,1 +1,10 @@
 export PATH=${PATH}:~/tools/depot_tools
+
+################################################################################
+## Beamer
+################################################################################
+
+# Put matlab in path on OSX
+if [ "$(uname)" == "Darwin" ]; then
+    export PATH=${PATH}:/Applications/MATLAB_R2014a.app/bin
+fi
