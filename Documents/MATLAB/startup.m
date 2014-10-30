b21b5e6cc18ee598b7819a9de356637126695bba
Michael Graczyk <michael@mgraczyk.com>
2014-10-30 01:02:52 -0700

    Default to OpenGL renderer in MATLAB

--- Documents/MATLAB/startup.m
+++ Documents/MATLAB/startup.m
@@ -2,6 +2,7 @@ home = getenv('HOME');
 matlabLibDir = fullfile(home, 'software/matlab');
 
 addpath(matlabLibDir)
+set(0, 'DefaultFigureRenderer', 'OpenGL');
 
 %devDir = fullfile(home, 'dev');
 %if exist(devDir, 'dir')
