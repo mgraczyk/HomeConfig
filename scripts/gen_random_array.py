26451180772a3a8769070167ccf066a00f027755
Michael Graczyk <michael@mgraczyk.com>
2015-01-12 18:00:42 -0800

    Update random array script to handle floating point values

--- scripts/gen_random_array.py
+++ scripts/gen_random_array.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python3
 
-import sys
 import argparse
+import math
+import sys
 
-def generate_elements_numpy(length, valMin, valMax):
-    return np.random.randint(int(valMin), int(valMax), int(length))
+def generate_elements_numpy(length, val_min, val_max, float_type):
+    if float_type:
+      val_range = np.nextafter(val_max - val_min, np.inf)
+      return val_range * np.random.rand(length) + val_min
+    else:
+      return np.random.randint(int(val_min), int(val_max), length)
 
-def generate_elements_std(length, valMin, valMax):
-    return [int(valMax*random.random()) + valMin for _ in range(length)]
+def generate_elements_std(length, val_min, val_max, float_type):
+    values = [
+        (math.floor(val_max) + 1)*random.random() + math.ceil(val_min)
+        for _ in range(length)]
+
+    if not float_type:
+        values = map(int, values)
+
+    return values
 
 def write_array_text(fp, name, dataType, data):
     fp.write("{} {}[] = {{ ".format(dataType, name))
@@ -18,19 +30,26 @@ def c_style_int(value):
     return int(value, 0)
 
 def get_arg_parser():
-    argParser = argparse.ArgumentParser(description='Generates an array of random data',
-            formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    argParser = argparse.ArgumentParser(
+        description='Generates an array of random data',
+formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    argParser.add_argument('-l', '--length', dest='length', type=c_style_int, default=1, help="Number of array elements.")
+    argParser.add_argument('-l', '--length', type=c_style_int, default=1,
+        help="Number of array elements.")
 
-    argParser.add_argument('-t', '--type', dest='data_type', default="unsigned char",
-            help="Array element type.")
+    argParser.add_argument('-t', '--data_type', default="unsigned char",
+        help="Array element type.")
 
-    argParser.add_argument('-n', '--name', dest='array_name', default="data",
-            help="Array element type.")
+    argParser.add_argument('-n', '--array_name', default="data",
+        help="Name of array variable.")
 
-    argParser.add_argument('-m', '--min', dest='val_min', type=c_style_int, default=0, help="Minimum data value.")
-    argParser.add_argument('-M', '--max', dest='val_max', type=c_style_int, default=256, help="Maximum data value.")
+    argParser.add_argument('-m', '--val_min', type=float, default=0,
+        help="Minimum data value.")
+    argParser.add_argument('-M', '--val_max', type=float, default=255,
+        help="Maximum data value.")
+
+    argParser.add_argument('-f', '--float_type', type=bool, default=None,
+        help="Maximum data value.")
 
     return argParser
 
@@ -40,7 +59,12 @@ def main():
         sys.stderr.write("ERROR: min must be less than or equal to max\n")
         exit(1)
 
-    data = generate_elements(args.length, args.val_min, args.val_max)
+    if args.float_type is None:
+      args.float_type = "float" in args.data_type or "double" in args.data_type
+
+    data = generate_elements(
+        args.length, args.val_min, args.val_max, args.float_type)
+
     write_array_text(sys.stdout, args.array_name, args.data_type, data)
 
 try:
@@ -51,7 +75,6 @@ except ImportError:
     import random
     np = None
     generate_elements = generate_elements_std
-
 
 if __name__ == "__main__":
     main()
