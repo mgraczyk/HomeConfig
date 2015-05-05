86ded310f7121dbbe022cfd8e64f1d864e4242b1
Michael Graczyk <michael@mgraczyk.com>
2015-05-04 18:14:56 -0700

    Add goma to chromium dev script

--- .chromium_dev.sh
+++ .chromium_dev.sh
@@ -14,3 +14,22 @@ export P4DIFF=vimdiff
 if [ -f ~/.google_dev.sh ] ; then
    source ~/.google_dev.sh
 fi
+
+# Goma setup
+function setup_goma() {
+  #export GOMA_DIR=~/tools/goma
+  #export CHROME_SRC=${HOME}/dev/chromium/src
+
+  #${GOMA_DIR}/goma_ctl.py ensure_start
+  #unset CC CXX
+
+  #cd ${CHROME_SRC}
+  #GYP_GENERATORS=ninja ./build/gyp_chromium -D use_goma=0 -D gomadir=${GOMA_DIR}
+  #export GYP_DEFINES="use_goma=0"
+  :;
+}
+
+function setup_chrome() {
+  setup_goma;
+  export CHROME_DEVEL_SANDBOX=/usr/local/sbin/chrome-devel-sandbox
+}
